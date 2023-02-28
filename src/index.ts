00626ac0ccd9445184646e4563d4ad4b98231d5c
cv <marcel.mimouni@gmail.com>
2023-02-28 14:48:22 +0100

    changement

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("coucou");
+console.log(`Node NODE_ENV  [${process.env.NODE_ENV}]`);
+console.log(`Node TZ  [${process.env.TZ}]`);
+console.log(`Node HOST  [${process.env.HOST}]`);
+
+console.log(`Node POT  [${process.env.PORT}]`);
